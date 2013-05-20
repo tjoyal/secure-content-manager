a286e619b0d399eeb8c1af5e79a1f07749e3568c
Thierry Joyal <thierry.joyal@gmail.com>
2013-05-20 08:10:56 -0400

    Update bootstrap helper header

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -1,18 +1,20 @@
 module BootstrapHelper
 
-  def bootstrap_header
+  def bootstrap_header(header = nil)
 
-    case action_name.to_sym
-      when :index
-        header = resource_class.model_name.human.pluralize
-      when :show
-        header = t '.title', :default => resource_class.model_name.human
-      when :edit
-        header = t '.title', :default => [:'helpers.titles.edit', 'Edit %{model}'], :model => resource_class.model_name.human
-      when :new
-        header = t '.title', :default => [:'helpers.titles.new', 'New %{model}'], :model => resource_class.model_name.human
-      else
-        raise "Undefined action named '#{action_name}'"
+    unless header
+      case action_name.to_sym
+        when :index
+          header = resource_class.model_name.human.pluralize
+        when :show
+          header = t '.title', :default => resource_class.model_name.human
+        when :edit
+          header = t '.title', :default => [:'helpers.titles.edit', 'Edit %{model}'], :model => resource_class.model_name.human
+        when :new
+          header = t '.title', :default => [:'helpers.titles.new', 'New %{model}'], :model => resource_class.model_name.human
+        else
+          raise "Undefined action named '#{action_name}'"
+      end
     end
 
     content_tag :div, :class => 'page-header' do
