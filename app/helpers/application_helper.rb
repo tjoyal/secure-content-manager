88dc2d434f8517aff8afcb901f0cadf2a3e3b1f6
Thierry Joyal <thierry.joyal@gmail.com>
2013-05-22 16:36:47 -0400

    Cleanup views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,15 @@ module ApplicationHelper
     f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
   end
 
-  def link_to_add_fields(name, f, association)
+  def link_to_add_fields(name, f, association, partial = nil)
+
+    unless partial
+      partial = association.to_s.singularize + "_fields"
+    end
+
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-      render(association.to_s.singularize + "_fields", :f => builder)
+      render(partial, :f => builder)
     end
     link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
   end
