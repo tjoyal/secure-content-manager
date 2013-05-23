70d0f1df32894c93c62b880b94c6d2961476f642
Thierry Joyal <thierry.joyal@gmail.com>
2013-05-23 17:08:31 -0400

    Replace section remove link with bootstrap buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,16 @@
 module ApplicationHelper
 
   def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+
+    html = []
+
+    html << f.hidden_field(:_destroy)
+
+    html << content_tag(:button, :type => :button, :class => ['btn', 'btn-danger', 'btn-mini'], :onclick => "remove_fields(this)") do
+      "Remove"
+    end.html_safe
+
+    html.join.html_safe
   end
 
   def link_to_add_fields(name, f, association, partial = nil)
