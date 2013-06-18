333c44871b9ea11cc555e4fee24b10568e941ee1
Thierry Joyal <thierry.joyal@gmail.com>
2013-06-18 07:15:33 -0400

    Add authorisations consideration and validation in Setting::Groups

--- app/controllers/setting/groups_controller.rb
+++ app/controllers/setting/groups_controller.rb
@@ -2,4 +2,6 @@ class Setting::GroupsController < ApplicationController
 
   inherit_resources
 
+  load_and_authorize_resource :group
+
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,12 +10,20 @@ class Ability
 
     if user && user.admin?
       can :manage, [Group, Server]
+      can :show, :group_settings
     else
       can :read, Group, :user_groups => {:user_id => user.id, :read => true}
+      can :update, Group, :user_groups => {:user_id => user.id, :manage => true}
 
       can :read,   Server, :group => { :user_groups => { :user_id => user.id, :read => true } }
       can :update, Server, :group => { :user_groups => { :user_id => user.id, :edit => true } }
       can :manage, Server, :group => { :user_groups => { :user_id => user.id, :manage => true } }
+
+      can :show, :group_settings do
+        user.user_groups.any? do |user_group|
+          user_group.manage
+        end
+      end
     end
 
   end
